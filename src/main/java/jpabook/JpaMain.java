a412e8858c6ad70ab132c711a57473ed18c2c1d5
Gongmeda <gongmeda@gmail.com>
2022-03-16 02:31:35 +0900

    04장 - 엔티티 매핑

--- src/main/java/jpabook/JpaMain.java
+++ src/main/java/jpabook/JpaMain.java
@@ -1,4 +1,7 @@
-package jpabook.start;
+package jpabook;
+
+import jpabook.model.Member;
+import jpabook.model.Order;
 
 import javax.persistence.*;
 import java.util.List;
@@ -27,28 +30,7 @@ public class JpaMain {
     }
 
     public static void logic(EntityManager em) {
-        String id = "id1";
-        Member member = new Member();
-        member.setId(id);
-        member.setUsername("지한");
-        member.setAge(2);
 
-        //등록
-        em.persist(member);
-
-        //수정
-        member.setAge(20);
-
-        //한 건 조회
-        Member findMember = em.find(Member.class, id);
-        System.out.println("findMember=" + findMember.getUsername() + ", age=" + findMember.getAge());
-
-        //목록 조회
-        List<Member> members = em.createQuery("select m from Member m", Member.class).getResultList();
-        System.out.println("members.size=" + members.size());
-
-        //삭제
-        em.remove(member);
     }
 
 }
